13324bd56353968e8e365efc0c3007471e5912d5
Neven Hsu <space.man0318@gmail.com>
2023-01-03 21:06:09 +0800

    add leva gui

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,63 @@
-import { useRef } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef, forwardRef } from 'react'
+import * as THREE from 'three'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { OrbitControls, Stats } from '@react-three/drei'
 import { EffectComposer, Bloom } from '@react-three/postprocessing'
+import { useControls } from 'leva'
 import { Model as Model01 } from 'models/Planet_01'
 import type { Group } from 'three'
+import type { OrbitControlsProps } from '@react-three/drei'
+import type { OrbitControls as OrbitControlsImpl } from 'three-stdlib'
+
+const Controls = forwardRef<OrbitControlsImpl, OrbitControlsProps>(
+  (props, ref) => {
+    const { gl, camera } = useThree()
+    return (
+      <OrbitControls
+        ref={ref}
+        makeDefault
+        target={[0, 0, -30]}
+        args={[camera, gl.domElement]}
+        {...props}
+      />
+    )
+  }
+)
 
 const Scene = () => {
-  const el = useRef<Group>(null)
+  const groupRef = useRef<Group>(null)
+  const { z } = useControls('Camera', {
+    z: {
+      value: 30,
+      min: 10,
+      max: 100,
+    },
+  })
+
   useFrame((state, delta) => {
-    if (el.current) {
-      el.current.rotation.y += delta / 2
-      el.current.rotation.z += delta / 8
+    if (groupRef.current) {
+      groupRef.current.rotation.y += delta / 2
+      groupRef.current.rotation.z += delta / 8
     }
   })
 
+  const pos = [0, 0, -z] as unknown as THREE.Vector3
+
   return (
-    <group ref={el}>
-      <Model01 />
-      <ambientLight intensity={3.5} />
-    </group>
+    <>
+      <Controls target={pos} />
+      <group ref={groupRef} position={pos}>
+        <Model01 />
+        <ambientLight intensity={3.5} />
+      </group>
+    </>
   )
 }
 
 const App = () => {
   return (
-    <Canvas camera={{ fov: 70, position: [0, 0, 30] }}>
+    <Canvas camera={{ fov: 70, position: [0, 0, 0] }}>
       <Stats />
-      <OrbitControls />
       <Scene />
       <EffectComposer>
         <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} />
